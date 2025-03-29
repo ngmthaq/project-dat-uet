8bd445bdb32ccce4eea75752d670c00cd358309a
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-03-29 10:56:43 +0700

    Test Swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,29 @@
 import { Body, Controller, Post, HttpCode, HttpStatus, Get, Req } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AuthRequest } from "@/@types";
 import { AuthService } from "./providers/auth.service";
 import { SkipAuth } from "./providers/skip-auth.decorator";
 import { LoginDto } from "./models/login.dto";
 
 @Controller("auth")
+@ApiTags("auth")
 export class AuthController {
   public constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @SkipAuth()
   @Post("login")
+  @ApiOperation({ summary: "Login", description: "Login to the system" })
+  @ApiResponse({ status: HttpStatus.OK, description: "Login successful" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Invalid credentials" })
   public login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Get("user")
+  @ApiOperation({ summary: "Auth user", description: "Get auth user profile" })
+  @ApiResponse({ status: HttpStatus.OK, description: "Success" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Unauthorize" })
   public getAuthUser(@Req() req: AuthRequest) {
     return this.authService.getAuthUser(req.user.id);
   }
--- src/auth/models/login.dto.ts
+++ src/auth/models/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: "john_doe" })
   public username: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: "securepassword1" })
   public password: string;
 }
